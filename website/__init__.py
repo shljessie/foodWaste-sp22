43087c2dd8726088dd7f0536c955f3a1670a94eb
Seunghee Lee <59305253+shljessie@users.noreply.github.com>
2022-04-06 21:44:41 -0400

    Add files via upload

---
+++ website/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'randomstring'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    # we have some blueprints that have dif views / urls, 
+    # here's where they are
+    from .views import views 
+    from .auth import auth
+
+    # register our blueprints with our Flask app
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note  
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+
+
